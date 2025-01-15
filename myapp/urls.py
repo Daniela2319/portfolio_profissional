a7aba3df6269a190f64c454fd7a5a9b9cac630c3
Daniela2319 <frankvt04@gmail.com>
2025-01-15 15:45:53 -0300

    modificações no portfólio

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from myapp.views import index, home_view, contato_view
+from myapp.views import index, home_view, contato_view, sobre_view, projeto_view
 
 urlpatterns = [
     path("", index, name="base"),
     path("home/", home_view, name="home"),
+    path("sobre/", sobre_view, name="sobre"),
+    path("projeto/", projeto_view, name="projeto"),
     path("contato/", contato_view, name="contato"),
 ]
