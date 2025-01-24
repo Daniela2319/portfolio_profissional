30e506914948b205e3a120decf5b45ccdf5fd039
Daniela2319 <frankvt04@gmail.com>
2025-01-24 13:21:14 -0300

    atualizaçãoA4

--- myprojeto/settings.py
+++ myprojeto/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pxk&^gr31!i9@duioutov2ionja@89adjexa+b5!kq0rbk-ao^"
-
+# Secret Key
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost"]
+DEBUG = config("DEBUG", default=False, cast=bool)
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS", default="localhost,127.0.0.1,.vercel.app"
+).split(",")
 
 
 # Application definition
