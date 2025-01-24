af6c96944acf0f6490cd672f246ff368ea4e7bab
Daniela2319 <frankvt04@gmail.com>
2025-01-24 16:09:29 -0300

    atualizaçãoA81

--- myprojeto/settings.py
+++ myprojeto/settings.py
@@ -143,6 +143,7 @@ STATICFILES_DIRS = [
 # Diretório onde os arquivos estáticos serão coletados (somente para produção)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
