8d67182b3de40c61a66b51247dc3497b9dff4244
Daniela2319 <frankvt04@gmail.com>
2025-01-26 10:13:05 -0300

    Adicionando configuração do Tailwind CSS

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+# Instalar dependências do Python
 python3 -m pip install -r requirements.txt
+
+# Instalar dependências do Node.js
+npm install
+
+# Rodar o build do Tailwind CSS
+npm run build:css
+
+# Coletar os arquivos estáticos do Django
 python3 manage.py collectstatic --no-input
+
 
 
 
